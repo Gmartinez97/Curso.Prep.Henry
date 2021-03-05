4e40d1fd1ef59b55d020c0c8473687c3c79d00e0
Gmartinez97 <gmartinezerio@hotmail.com>
2021-03-04 21:17:40 -0300

    homework jsii

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -90,7 +90,7 @@ function esDiezOCinco(numero) {
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
-  // De lo contrario, devuelve "false"
+  // De lo contrario, devuelve "false"n
   // Tu código:
   return numero <50 && numero >20
 }
